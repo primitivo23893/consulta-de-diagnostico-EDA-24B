9052f4612ed18c1ccba16207de299a587349c2e1
primitivo23893 <oyoquevargas@gmail.com>
2024-11-12 16:12:38 -0600

    commit

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,39 @@
 #include "menu.h"
+#include "Cultivo.h"
+
 #include <iostream>
 using namespace std;
+#ifdef _WIN32
+#define CLEAR "cls"  // Comando para limpiar pantalla en Windows
+#else
+#define CLEAR "clear"  // Comando para limpiar pantalla en otros SO
+#endif
+void registroCultivo();
+void mostrarMenu();
 
 Menu::Menu(){
     folioGlobal = 1;
+    bool seguir = true;
+    int opc;
     //main
-    cout << "Hola mundo!!" << endl;
+
+    do
+    {
+        system(CLEAR); //Borra toda la pantalla
+        mostrarMenu();
+        cin >> opc;
+
+        switch (opc){
+        case 1:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+
+    } while (seguir);
+    
 
 }
 int Menu::getFolio(){
@@ -17,4 +45,16 @@ void Menu::incrementarFolio(){
 }
 void Menu::setFolio(int folio){
     folioGlobal = folio;
+}
+
+void registroCultivo(){
+    Cultivo cultivo;
+    string nombre;
+
+    cout << "Dame tu nombre:";
+    getline(cin, nombre);
+
+}
+void mostrarMenu(){
+    
 }
